caf974f849fb876ca6809ba843defef513337316
Apeksha Malik <malikapeksha@gmail.com>
2025-03-27 16:51:58 -0400

    QuestionView

---
+++ QuizApp/QuestionView.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionView.swift
+//  QuizApp
+//
+//  Created by Apeksha Malik on 3/27/25.
+//
+
+import SwiftUI
+
+struct QuestionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuestionView()
+}
