7e9d8ffe1b9893c39b9fc49dc997a239e625e651
Vidyut Veedgav <vidyut.veedgav@gmail.com>
2025-03-27 16:17:09 -0400

    init
    Signed-off-by: Vidyut Veedgav <vidyut.veedgav@gmail.com>

---
+++ QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by Vidyut Veedgav on 3/27/25.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
