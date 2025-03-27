f3480cde3a99f7cbf195315e311b21f49efad833
Apeksha Malik <malikapeksha@gmail.com>
2025-03-27 17:00:31 -0400

    question view
    Signed-off-by: Apeksha Malik <malikapeksha@gmail.com>

--- QuizApp/QuestionView.swift
+++ QuizApp/QuestionView.swift
@@ -6,10 +6,43 @@
 //
 
 import SwiftUI
+import Foundation
+
+struct Question: Identifiable {
+    let id = UUID()
+    let text: String
+    let answers: [String]
+    let correct: String
+}
 
 struct QuestionView: View {
+    @State private var currentQuestionIndex = 0
+    @State private var selectedAnswer: String? = nil
+    @State private var incorrectCount = 0
+    @State private var showScore = false
+    
+    @State private var questions = [Question(text: "In SwiftUI, which type of stack is used for layering views on top of each other?",
+        answers: ["ZStack", "VStack", "LStack", "HStack"],
+        correct: "ZStack")]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if showScore {
+                Text("Score")
+                    .font(.largeTitle)
+                Text("Incorrect Answers: \(incorrectCount)")
+                    .font(.title)
+            }
+            else {
+                Text("Quizzo")
+                    .font(.largeTitle)
+                    .bold()
+                
+                Text(questions[currentQuestionIndex].text)
+                                   .font(.headline)
+                                   .padding()
+            }
+        }
     }
 }
 
